b413759a422060e344450f312abd8a4902b42bd2
wjunhee102 <wjunhee102@naver.com>
2020-02-07 10:16:11 +0900

    변경

--- src/elements/intro.js
+++ src/elements/intro.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import { motion } from "framer-motion"
-
-function MainVisual() {
-    const [y, setY] = useState(0);
-    const [s, setS] = useState(1);
-    const [op, setOp] = useState(1);
-    const [ticking, setTicking] = useState(false);
-
-    function move(scroll_pos) {
-        if (scroll_pos < 800) {
-            setY(scroll_pos);
-            if (scroll_pos <= 100) {
-            setS(1);
-            setOp(1);
-            } else {
-            setS(0.3);
-            setOp(0);
-            }
-        }
-    }
-    window.addEventListener('scroll', function(e) {
-        let pos = window.scrollY;
-        if (!ticking) {
-          window.requestAnimationFrame(function() {
-            move(pos);
-            setTicking(false);
-          });
-          setTicking(true);
-        }
-      });
-    return (
-        <section className="intro"> 
-            <motion.div className="main_visual" animate={{y : y , scale : s}} transition={{duration : 1}}>
-                <h2 className="tit" style={{opacity : op, transition: '0.5s'}}>JUN HEE</h2>
-            </motion.div>
-=======
 import React, { useState } from 'react';
 
 function MainVisual({valueY, valueS, valueOp, valuePos}) {
@@ -46,7 +8,6 @@ function MainVisual({valueY, valueS, valueOp, valuePos}) {
                     <h2 className="tit" style={{opacity : valueOp, transition: `0.5s`}}>HELLO, WORLD</h2>
                 </div>
             </div>
->>>>>>> face58087d75cbf611f77413958c1ea62a36bcfd
         </section>
     )
 }
