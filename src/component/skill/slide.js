9c9e2a83f6218a5195702c273011168e007de7c6
wjunhee102 <wjunhee102@naver.com>
2020-02-18 05:34:47 +0900

    slide 루프 및 마우스 무브 애니메이션 제작

--- src/component/skill/slide.js
+++ src/component/skill/slide.js
@@ -38,7 +38,8 @@ function Slide() {
         [slideIdx, setSIdx] = useState(0),
         [slideStart, setSStart] = useState(true),
         [sWrap, setWrap] = useState(0),
-        [enter, setEnter] = useState(false);
+        [enter, setEnter] = useState(false),
+        [sWrapW, setWW] = useState(0)
         ;
     const
         slideWidth = useCallback(node => {
@@ -50,9 +51,21 @@ function Slide() {
         slideWrap = useCallback(node => {
             if (node !== null) {
                 setWrap(node);
+                setWW(node.getBoundingClientRect().width);
             }},[])
             ;
+ 
 
+    let 
+        mouseUp = false,
+        mouseX = 0,
+        mouseSX = 0,
+        inum = 0,
+        wrapMove = 0,
+        ss = false,
+        mouseEnter = true
+        ;
+     
     
 
     function slideOn(x) {
@@ -62,50 +75,41 @@ function Slide() {
             return ''
             }
         }
-    function slideMouseEvent() {
-        let 
-        mouseUp = false,
-        mouseX = 0,
-        mouseSX = 0,
-        inum = 0,
-        wrapMove = 0
-        ;
-
-        sWrap.addEventListener("mousedown", (e)=>{
-            setSStart(false);
-            mouseUp = true
-            mouseSX = e.clientX;
-            sWrap.addEventListener("mousemove",(e)=>{
-                if(!mouseUp) return false
-                    mouseX = e.clientX - mouseSX  ;
-                    wrapMove = slideMove - mouseX
-                    sWrap.style.transform = `translateX(${-wrapMove}px)`
-                    
-            })
-        })
-        sWrap.addEventListener("mouseup", ()=>{
-            setSMove(wrapMove);
-            setSStart(true);
-            mouseUp = false;
-            inum = Math.round(wrapMove/slideW);
-            console.log(wrapMove);
-            moveing(inum, wrapMove);
-            mouseX = 0;
-            mouseSX = 0;
-            inum = 0;
-            wrapMove = 0
-        })
+    
+    function slideEvent() {
+        mouseUp = false;
+        mouseEnter = true;
     }
+    function slideDown(e) {
+        mouseUp = true;
+        mouseEnter = false;
+        mouseSX = e.clientX;
+    }
+    function slideMoving(e) {
+        if(!mouseUp) return false
+        mouseX = e.clientX - mouseSX ;
+        wrapMove = slideMove - mouseX ;
+        sWrap.style.transform = `translateX(${-wrapMove}px)`
+    }
+
+    function slideStop() {
+        mouseUp = false;
+        if(!mouseEnter) {
+            inum = Math.round(wrapMove/slideW);
+            moveing(inum, wrapMove);
+        }
+    }
+
     
     function moveing(num, x) {
-        setSStart(true);
+        ss = true;
         let 
             i = slideW*num,
             start = i - x,
             startTime = null
             ;
         function easeOut (t, b, c, d) {
-            return c * ( -Math.pow( 2, -12 * t/d ) + 1 ) + b;
+            return c * ( -Math.pow( 2, -10 * t/d ) + 1 ) + b;
         };
 
         //animate
@@ -113,42 +117,52 @@ function Slide() {
             
             if(!startTime) startTime = timestamp
             let progress = timestamp - startTime
-            let moving = easeOut(progress, x, start, 2500)
-            sWrap.style.transform = `translateX(${-moving}px)`
-            if (slideStart) {
-                if (progress < 2500) {
+            let moving = easeOut(progress, x, start, 2000)
+            setSMove(moving);
+            if (ss) {
+                if (progress < 2000) {
                     requestAnimationFrame(animate);
                 } else {
-                    setSMove(i);
-                    sWrap.style.transform = `translateX(${-i}px)`
+                    if(i <= -slideW) {
+                        setSMove(sWrapW-slideW);
+                        setSIdx(slide_item.length-1);
+                    } else if(i >= sWrapW){
+                        setSMove(0);
+                        setSIdx(0);
+                    } else {
+                        setSMove(i);
+                        setSIdx(num);
+                    }
                 }
             }
         }
         window.requestAnimationFrame(animate);
     }    
-    // function init() {
-    //     sWrap.addEventListener('mouseenter', slideMouseEvent)
-    //     sWrap.addEventListener('mouseleave', ()=>{
-    //         mouseUp = false;
-    //     })
-    // }
 
     return (
         <div className="main_slide" >
 
             <div 
                 className="slide_wrap" 
+                style={{transform : `translateX(${-slideMove}px)`}}
                 ref={slideWrap}
-                onMouseEnter={slideMouseEvent}
+                onMouseEnter={slideEvent}
+                onMouseDown={e=>(
+                    e.preventDefault,
+                    slideDown(e)
+                    )}
+                onMouseMove={slideMoving}
+                onMouseUp={slideStop}
+                onMouseLeave={slideStop}
             >
-                {slideIdx === 0 ? (
+                {slideMove <= 0 ? (
                 <div ref={slideWidth} className={`slide_item item4 vir`}>{slide_item[4].title}</div>):(
                     null
                 )}
                 {slide_item.map((ele, idx)=>(
                     <div ref={slideWidth} className={`slide_item item${idx} ${slideOn(idx)}`} key={idx}>{ele.title}</div>
                 ))}
-                {slideIdx === 4 ? (
+                {slideMove >= sWrapW-slideW ? (
                 <div ref={slideWidth} className={`slide_item item0 vir2`}>{slide_item[0].title}</div>):(
                     null
                 )}
@@ -163,7 +177,7 @@ function Slide() {
                             onClick={(e)=>(
                             e.preventDefault,
                             setSStart(true),
-                            setSMove(slideW*idx),
+                            setSMove(slideW*idx+1),
                             setSIdx(idx),
                             moveing(idx, slideMove)
                             )}
