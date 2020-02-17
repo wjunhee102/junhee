c08f69cce2d8d521bbd8485db88fe6b836e9dd81
wjunhee102 <wjunhee102@naver.com>
2020-02-17 11:07:18 +0900

    슬라이드 수정중

--- src/component/skill/slide.js
+++ src/component/skill/slide.js
@@ -35,7 +35,8 @@ function Slide() {
     const 
         [slideMove, setSMove] = useState(0),
         [slideW, setSW] = useState(0),
-        [slideIdx, setSIdx] = useState(0)
+        [slideIdx, setSIdx] = useState(0),
+        [slideStart, setSStart] = useState(true)
         ;
     const
         slideWidth = useCallback(node => {
@@ -43,6 +44,12 @@ function Slide() {
                 setSW(node.getBoundingClientRect().width);
             }},[])
             ;
+    let 
+        mouseX = 0,
+        mouseSX = 0,
+        mouseUp = true
+        ;
+
     function slideOn(x) {
         if(slideIdx == x) {
             return "on"
@@ -50,13 +57,66 @@ function Slide() {
             return ''
             }
         }
-        
+    
+    function slideMouseEvent() {
+    
+        window.addEventListener("mousedown", (e)=>{
+            mouseUp = true
+            mouseSX = e.clientX;
+            window.addEventListener("mousemove",(e)=>{
+                if(mouseUp) {
+                    mouseX = mouseSX - e.clientX
+                    setSMove(slideMove + mouseX);
+                } 
+            })
+            
+        })
+        window.addEventListener("mouseUp", ()=>{
+            mouseUp = false
+        })
+    }
+
+    function moveing(num) {
+        setSStart(true);
+        let 
+            i = slideW*num,
+            start = i - slideMove,
+            startTime = null
+            ;
+        function easeOut (t, b, c, d) {
+            return c * ( -Math.pow( 2, -12 * t/d ) + 1 ) + b;
+        };
+
+        //animate
+        function animate(timestamp) {
+            
+            if(!startTime) startTime = timestamp
+            let progress = timestamp - startTime
+            let moving = easeOut(progress, slideMove, start, 2500)
+
+            setSMove(moving);
+            if (slideStart) {
+                if (progress < 2500) {
+                    requestAnimationFrame(animate);
+                } else {
+                    setSMove(i);
+                }
+            }
+        }
+        window.requestAnimationFrame(animate);
+
+        console.log(i)
+    }    
         
 
     return (
         <div className="main_slide">
 
-            <div className="slide_wrap" style={{transform : `translateX(${-slideMove}px)`, transition : `0.5s ease-in-out`}}>
+            <div 
+                className="slide_wrap" 
+                style={{transform : `translateX(${-slideMove}px)`}}  
+                
+                >
                 {slideIdx === 0 ? (
                 <div ref={slideWidth} className={`slide_item item4 vir`}>{slide_item[4].title}</div>):(
                     null
@@ -79,7 +139,8 @@ function Slide() {
                             onClick={(e)=>(
                             e.preventDefault,
                             setSMove(slideW*idx),
-                            setSIdx(idx)
+                            setSIdx(idx),
+                            moveing(idx)
                             )}
                         >
                             {ele.content}
