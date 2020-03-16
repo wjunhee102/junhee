9a7a311fa729d55676065357357068ce6a2d13f0
wjunhee102 <wjunhee102@gmail.com>
2020-03-17 07:53:25 +0900

    connect -> contact로 이름 변경

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './css/connect.css';
+import './css/contact.css';
 
 
 // function useInput(defaultValue) {
@@ -62,21 +62,31 @@ import './css/connect.css';
 //     )
 // }
 
-const connect_info = {
+const contact_info = {
     name : "황준희",
     phone : "010-9878-8227",
     email : "wjunhee102@naver.com",
     cv : ""
 }
 
-function Connect() {
+function Contact() {
     return (
         <>
-        <section className="connect">
+        <section className="contact">
             <div className="inner">
-                <h2 className="name">{connect_info.name}</h2>
-                <p className="phone">{connect_info.phone}</p>
-                <p className="email">{connect_info.email}</p>
+                <p className="closing">
+                    <span>마치며...</span>
+                    이 포트폴리오 사이트는 React로 만들었습니다.
+                    <br />포트폴리오 만들기 시작했을 때 리액트에 관련된 동영상을 보았었고 왠지 저도 만들 수 있을것 같았습니다. 
+                    <br />하지만 생각보다 어려웠습니다. 
+                    <br />특히 Hooks이 어떻게 동작하는 아직도 이걸 완성한 지금도 잘 모르겠습니다.
+                    <br />저의 부족함을 많이 느꼈습니다.  그래서 저는 더욱 배우고 싶어졌습니다.  선배 개발자들은 어떻게 자바스크립트를 다루고 어떻게 웹을 만드는지 배우고 싶습니다. 그리고 현업에서는 어떻게 리액트를 사용하는지 배우고 싶습니다.  
+                </p>
+                <div className="contact_info">
+                    <h2 className="name">{contact_info.name}</h2>
+                    <p className="phone">{contact_info.phone}</p>
+                    <p className="email">{contact_info.email}</p>
+                </div>
             </div>
         </section>
         <footer id="footer">
@@ -91,4 +101,4 @@ function Connect() {
 
 
 
-export default Connect; 
+export default Contact; 
