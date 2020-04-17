81a967dee6531580e9a9866e398bdde9b19818a9
wjunhee102 <wjunhee102@gmail.com>
2020-04-17 17:48:47 +0900

    오류 수정 필요

--- src/component/hooks/useHeight.js
+++ src/component/hooks/useHeight.js
@@ -3,13 +3,14 @@ import { useState, useRef, useEffect } from 'react';
 function useHeight() {
     const [height, setHeight] = useState(0);
     const value = useRef();
+    const aaa = window.outerHeight
 
     useEffect(()=>{
         if(value) {
             const { current } = value;
             setHeight(current.getBoundingClientRect().height);
         }
-    },[])
+    },[aaa])
 
     useEffect(()=>{
         window.addEventListener('resize',()=>{
